54756420113f60341f08842fcc6e946a7f9122b9
skh <seogkihong333@gmail.com>
2023-04-18 22:55:18 +0900

    mongoDB schema 밎 koa crud / middleware 이해

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
 import api from './api';
+import createFakeData from './createFakeData';
+import jwtMiddleware from './lib/jwtMiddleware';
+
 const app = new Koa();
 const router = new Router();
 
@@ -13,6 +16,7 @@ mongoose
   .connect(MONGO_URL)
   .then(() => {
     console.log('Connected To MongoDB');
+    // createFakeData();
   })
   .catch((e) => {
     console.error(e);
@@ -21,6 +25,7 @@ mongoose
 router.use('/api', api.routes());
 
 app.use(bodyParser());
+app.use(jwtMiddleware);
 app.use(router.routes()).use(router.allowedMethods());
 const port = PORT || 4000;
 app.listen(port, () => {
